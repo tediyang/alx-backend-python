a43bdc918275111f6ca793d6d2773a779c7494b0
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-15 14:51:53 +0100

    add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    A function that adds two floats.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        add: returns the addition of two floats.
+
+        Args:
+            a (float): float number
+            b (float): float number
+
+        Returns:
+            float: addition of params
+    """
+    return a + b
