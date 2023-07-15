35eb12ebd518f90056063ace4bceee32b4d7a44c
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-15 15:27:39 +0100

    floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+
+"""
+    a function that returns the floor of a number.
+"""
+
+
+def floor(n: float) -> int:
+    """
+        floor: returns the floor of a float
+
+        Args:
+            n (float): float integer
+
+        Returns:
+            int: returns the floor of the number
+    """
+    return round(n)
