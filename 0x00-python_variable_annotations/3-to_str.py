ae444e8365c5aeaf54f0c9081b6990c8261d0e2b
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-15 15:59:04 +0100

    to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+
+"""
+    a function that takes a float as arg and returns a string.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        to_str: returns a str format of the arg
+
+    Args:
+        n (float): float integer
+
+    Returns:
+        str: string format of the arg.
+    """
+    return str(n)
