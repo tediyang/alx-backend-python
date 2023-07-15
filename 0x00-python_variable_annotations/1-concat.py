719f8d49d3cd8d49a93c5d3e8bc0a152706fb474
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-15 15:07:11 +0100

    concat function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    a function that returns concatenated strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        concat: returns two strings concatenated.
+
+    Args:
+        str1 (str): first string.
+        str2 (str): second string.
+
+    Returns:
+        str: concatenated string
+    """
+    return str1 + str2
