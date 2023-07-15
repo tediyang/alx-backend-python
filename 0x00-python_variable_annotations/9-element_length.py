6a4ee657e5c9a0829dfba1f1f6ac70b112703501
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-15 20:10:23 +0100

    element_length: using typing sequence and iterable

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    a function that return a list of index and item.
+"""
+from typing import Sequence, Iterable
+from typing import Tuple, List
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence]]:
+    """
+        element_length: returns a list of index and item.
+
+    Args:
+        lst (Iterable[Squence]): Iterable Sequence
+
+    Returns:
+        List[Tuple[Sequence]]: returns a list containing a tuple(iterable)
+    """
+    return [(i, len(i)) for i in lst]
