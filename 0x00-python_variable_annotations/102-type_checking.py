49604ac36e3f8836531b398f490f96786717bf2f
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-15 22:15:34 +0100

    change type to tuple

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,7 +5,7 @@
 from typing import Tuple, List
 
 
-def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
         zoon_array: returns a list generated from a tuple.
 
@@ -16,11 +16,11 @@ def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
         Returns:
             List: returns a list
     """
-    zoomed_in: List[int] = [item for item in lst for _ in range(factor)]
+    zoomed_in: List = [item for item in lst for _ in range(factor)]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
