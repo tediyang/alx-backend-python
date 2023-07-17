3ec9e5989bfea7e5e177a92e21c4408f80f4afef
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-17 12:03:29 +0100

    measure runtime: correctly labelled output

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int) -> int:
+def measure_time(n: int, max_delay: int) -> float:
     """
         measure_time: returns the average time taken to finish the process.
 
