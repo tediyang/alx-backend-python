9cc4331d67181c337e3a822ddfa5d84cfc052550
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-17 11:54:12 +0100

    commented random seed

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -16,7 +16,7 @@ async def wait_random(max_delay: int = 10) -> float:
         Returns:
             int: the generated value
     """
-    random.seed(444)  # whenever this code runs it returns the same values
+    # random.seed(444)  # whenever this code runs it returns the same values
     value = random.uniform(0, max_delay + 1)
     await asyncio.sleep(value)
     return value
