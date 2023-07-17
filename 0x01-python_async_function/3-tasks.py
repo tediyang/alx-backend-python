713919423a6d6841c96624fb5ab0c4b13714f676
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-17 12:44:53 +0100

    function that returns async Task: properly label the func and param

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,14 +8,14 @@ from typing import Coroutine
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_depth: int) -> Coroutine:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
         task_wait_random: return an asyncio.Task generated from wait_random
 
         Args:
-            max_depth (int): the maximum number the async function will sleep
+            max_delay (int): the maximum number the async function will sleep
 
         Returns:
             IO: an asyncio.Task
     """
-    return asyncio.create_task(wait_random(max_depth))
+    return asyncio.create_task(wait_random(max_delay))
