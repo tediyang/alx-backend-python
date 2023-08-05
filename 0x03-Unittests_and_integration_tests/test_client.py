22c6fb4c81cbbb0090df32b2db3ab050f2f503ea
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-05 21:33:53 +0100

    added public repo url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
     unittest for client module using parameterized.
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -26,8 +26,19 @@ class TestGithubOrgClient(unittest.TestCase):
 
             Args:
                 org (str): the org name
+                mock_js (obj): mocked data
         """
         endpoint = GithubOrgClient.ORG_URL.format(org=org)
         model = GithubOrgClient(org)
         model.org()
         mock_js.assert_called_once_with(endpoint)
+
+    @parameterized.expand([
+        ("random-url", {'repos_url': 'http://some_url.com'})
+    ])
+    def test_public_repos_url(self, name, result):
+        """ Test method returns correct output """
+        with patch('client.GithubOrgClient.org',
+                   PropertyMock(return_value=result)):
+            response = GithubOrgClient(name)._public_repos_url
+            self.assertEqual(response, result.get('repos_url'))
